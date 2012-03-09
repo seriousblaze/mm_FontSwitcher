87fafe426b3df173c2c0ddff667574a80642aa01
Serious Blaze <seriousblaze@gmail.com>
2012-03-09 16:10:54 +0630

    added About menu item

--- mm_FontSwitcher.py
+++ mm_FontSwitcher.py
@@ -18,8 +18,8 @@ class aStatusIcon:
         out_file.close ()
         return out
             
-        #    def click_event(self, icon, button, time):
-    def click_event( self,  statusicon):
+        # def click_event(self, icon, button, time):
+    def click_event( self, statusicon):
 
         self.menu = Gtk.Menu()
 
@@ -27,6 +27,8 @@ class aStatusIcon:
         unicode_item.set_label("Unicode")
         zgy_item = Gtk.MenuItem()
         zgy_item.set_label("Zawgyi-One")
+        about_item = Gtk.MenuItem()
+        about_item.set_label("About")
         quit_item = Gtk.MenuItem()
         quit_item.set_label("Quit")
 
@@ -42,6 +44,7 @@ class aStatusIcon:
 
         unicode_item.connect("activate", layout_toggle)
         zgy_item.connect("activate", layout_toggle)
+        about_item.connect("activate", self.show_about_dialog)
         quit_item.connect("activate", Gtk.main_quit)
 
         out = self.get_layout()
@@ -49,7 +52,8 @@ class aStatusIcon:
             self.menu.append(unicode_item)
         else:
             self.menu.append(zgy_item)
-        
+
+        self.menu.append(about_item)
         self.menu.append(quit_item)
 
         self.menu.show_all()
@@ -58,6 +62,20 @@ class aStatusIcon:
             return (Gtk.StatusIcon.position_menu(menu, icon))
 
         self.menu.popup(None, None, pos, self.statusicon, 0, Gdk.CURRENT_TIME)
+
+    def show_about_dialog(self, widget):
+        about_dialog = Gtk.AboutDialog()
+
+        about_dialog.set_destroy_with_parent(True)
+        about_dialog.set_name("MM_FontSwitcher")
+        about_dialog.set_version("1.0")
+        about_dialog.set_authors(["Naing Ye` Minn", "Thura Hlaing"])
+        about_dialog.set_website("http://seriousblaze.u4mm.com")
+        about_dialog.set_website_label("http://seriousblaze.u4mm.com")
+        about_dialog.set_comments("This application can switch the default fallback font \nbetween Zawgyi-One and Myanmar Unicode (TharLon) \nin Ubuntu 11.10 (Unity).")
+
+        about_dialog.run()
+        about_dialog.destroy()
 
 aStatusIcon()
 Gtk.main()
